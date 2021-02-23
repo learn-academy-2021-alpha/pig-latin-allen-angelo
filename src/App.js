c1ce65a1da4360e68fb46629e05edf25bbb987e6
algtan <agtan@ymail.com>
2021-02-23 14:18:32 -0800

    translator works with all caps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-
+import AudioPlayer from './components/AudioPlayer'
 
 
 class App extends Component {
@@ -12,7 +12,9 @@ class App extends Component {
       // ACTION ITEM: when you are ready for your full user experience, delete the test words so phrase is assigned an empty string
       phrase: "alpha through yummy squeal queen fry",
       // "phraseTranslated" is what the user will see appear on the page as Pig Latin, it starts as the preset message and updates when your user clicks the "submit" button
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: "This is where your translated sentence will appear.",
+      // "translatedFontColor" - red when not translated, green when translated
+      translatedFontColor: "red"
     }
   }
 
@@ -49,40 +51,51 @@ class App extends Component {
       // Case 1 - Word starts with a vowel, add way at the end
       if ( vowels.includes(wordArray[0].toLowerCase()) ) {
         // Return a string of the current word plus 'way'
-        return currentWord + 'way'
-      } else if (!currentWord.includes("a") && !currentWord.includes("e") && !currentWord.includes("i") && !currentWord.includes("o") && !currentWord.includes("u")) {
+        if ( currentWord === currentWord.toUpperCase() ) {
+          return currentWord + 'WAY'
+        } else {
+          return currentWord + 'way'
+        }
+      // Case 2 - Sometimes 'y'
+      } else if (!currentWord.toLowerCase().includes("a") && !currentWord.toLowerCase().includes("e") && !currentWord.toLowerCase().includes("i") && !currentWord.toLowerCase().includes("o") && !currentWord.toLowerCase().includes("u")) {
 
         for(let i = 0; i < wordArray.length; i++) {
-          if("y".includes(wordArray[i])) {
+          if("y".includes(wordArray[i].toLowerCase())) {
             vowelIndex = i
             // vowelIndex = currentWord.indexOf(char)
             break
           }
         }
         //return final string
-        return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "ay"
-    }else{
-//return a new string with the begininng consonents at the end + 'ay'
-    for(let i = 0; i < wordArray.length; i++) {
-      if(vowels.includes(wordArray[i])) {
-        if (wordArray[i-1] === "q" && wordArray[i] === "u") {
-          continue
+        if ( currentWord === currentWord.toUpperCase() ) {
+          return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "AY"
+        } else {
+          return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "ay"
         }
-        vowelIndex = i
-        // vowelIndex = currentWord.indexOf(char)
-        break
+      } else {
+        //return a new string with the begininng consonents at the end + 'ay'
+        for(let i = 0; i < wordArray.length; i++) {
+          if(vowels.includes(wordArray[i].toLowerCase())) {
+            if (wordArray[i-1].toLowerCase() === "q" && wordArray[i].toLowerCase() === "u") {
+              continue
+            }
+            vowelIndex = i
+            break
+          }
+        }
+        //return final string
+        if ( currentWord === currentWord.toUpperCase() ) {
+          return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "AY"
+        } else {
+          return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "ay"
+        }
       }
-    }
-//return final string
-  return currentWord.slice(vowelIndex) + currentWord.slice(0, vowelIndex) + "ay"
-
-    }
 
 
 
       // ACTION ITEM: change the value of currentWord to the name of whatever variable you made containing your Pig Latin'd word
       // return currentWord
-  })
+    })
 
 
     // joining the array back to a string of translated words
@@ -92,7 +105,10 @@ class App extends Component {
 
     // the setState method will take your information from "translatedWords" and update the state object that is displayed to the user
     // no need to change this method
-    this.setState({ phraseTranslated: translatedWords })
+    this.setState({
+      phraseTranslated: translatedWords,
+      translatedFontColor: "green"
+    })
   }
 
   restartGame = () => {
@@ -100,7 +116,8 @@ class App extends Component {
     // ACTION ITEM: when you are ready for your full user experience, delete the test words in phrase so that is assigned an empty string
     this.setState({
       phrase: "alpha through yummy squeal queen fry",
-      phraseTranslated: "This is where your translated sentence will appear."
+      phraseTranslated: "This is where your translated sentence will appear.",
+      translatedFontColor: "red"
     })
   }
 
@@ -121,6 +138,9 @@ class App extends Component {
     return (
       <>
         <h1>Pig Latin Translator</h1>
+        <h3 id="musicText">Play Me!</h3>
+        <AudioPlayer />
+        <br />
         <img
           src="https:www.holidogtimes.com/wp-content/uploads/2017/12/micro-pig-myth-cover.png"
           alt="pig with butcher cut names in pig latin"
@@ -140,8 +160,9 @@ class App extends Component {
           <button onClick={ this.setUpPreventDefault }>Submit</button>
           <button onClick={ this.restartGame }>Clear</button>
         </div>
-        <p id="output"
-        >{ this.state.phraseTranslated }</p>
+        <p id="output" style={{color: this.state.translatedFontColor}}>
+          { this.state.phraseTranslated }
+        </p>
         <footer>Coded by ~Allen & Angelo</footer>
       </>
     )
---
+++ src/components/AudioPlayer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+// import mp3_file from './sounds/0010_beat_egyptian.mp3';
+
+const AudioPlayer = function(props) {
+  return (
+    <audio src="https://ia800408.us.archive.org/27/items/HaloThemeSongOriginal/Halo%20Theme%20Song%20Original.mp3" controls autoplay />
+  );
+}
+export default AudioPlayer;
